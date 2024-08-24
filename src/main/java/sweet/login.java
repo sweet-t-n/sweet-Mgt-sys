e1efdd142f45a95231dfdb72ab1734b4b7d871ed
tasneem-ratrout <s12112159@stu.najah.edu>
2024-08-25 00:37:21 +0300

    Update login.java

--- src/main/java/sweet/login.java
+++ src/main/java/sweet/login.java
@@ -1,15 +1,20 @@
 package sweet;
 
-
-
 public class login {
     private String username;
     private String password;
     private boolean loggedIn;
 
-    // Constructor
+    // Default Constructor
     public login() {
         this.loggedIn = false; // Initially, no user is logged in
+    }
+
+    // Constructor with credentials and login attempt
+    public login(String inputUsername, String inputPassword) {
+        this(); // Call default constructor to initialize loggedIn
+        setCredentials(inputUsername, inputPassword);
+        this.loggedIn = validateCredentials(inputUsername, inputPassword);
     }
 
     // Set user credentials
@@ -21,17 +26,6 @@ public class login {
     // Validate credentials against the current username and password
     private boolean validateCredentials(String inputUsername, String inputPassword) {
         return inputUsername.equals(this.username) && inputPassword.equals(this.password);
-    }
-
-    // Attempt to log in with the provided username and password
-    public boolean login(String inputUsername, String inputPassword) {
-        if (validateCredentials(inputUsername, inputPassword)) {
-            this.loggedIn = true;
-            return true;
-        } else {
-            this.loggedIn = false;
-            return false;
-        }
     }
 
     // Check if the user is currently logged in
