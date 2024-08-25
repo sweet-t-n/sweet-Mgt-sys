9710354f8f820aa23113d9d316f65dd3aee101ef
tasneem-ratrout <s12112159@stu.najah.edu>
2024-08-25 13:48:13 +0300

    Update MyApplication.java

--- src/main/java/sweet/MyApplication.java
+++ src/main/java/sweet/MyApplication.java
@@ -5,15 +5,13 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
-//import java.util.logging.Logger;
+
 
 public class MyApplication {
-    //private static final Logger logger = Logger.getLogger(MyApplication.class.getName());
+  
 
     private static ArrayList<User> userList = new ArrayList<>();
-    //private static ArrayList<StoreOwner> storeOwnerList = new ArrayList<>();
-   // private static ArrayList<Admin> adminList = new ArrayList<>();
-   // private static ArrayList<MaterialSupplier> materialSupplierList = new ArrayList<>();
+   
     private Set<String> registeredUsers = new HashSet<>();
     private boolean loggedIn = false;
 
@@ -76,21 +74,21 @@ public class MyApplication {
                     userList.add(new User(data[1], data[2], data[3], data[4]));
                     break;
                 case "StoreOwner":
-                   // storeOwnerList.add(new StoreOwner(data[1], data[2], data[3], data[4]));
+                  
                     break;
                 case "Admin":
-                   // adminList.add(new Admin(data[1], data[2], data[3], data[4]));
+                   
                     break;
                 case "MaterialSupplier":
-                 //   materialSupplierList.add(new MaterialSupplier(data[1], data[2], data[3], data[4]));
+                
                     break;
                 default:
-                   // logger.warning(String.format("Unknown user type: %s", data[0]));
+                  
                     break;
             }
         }
     } catch (FileNotFoundException e) {
-      //  logger.severe(String.format("Error loading user data: %s", e.getMessage()));
+      
     }
 }
 }
